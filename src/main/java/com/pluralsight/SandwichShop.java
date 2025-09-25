50c00b48099bc8fa2faaef6f7c3d18e53a54b914
Zain Ashraf <zainashraf1562@gmail.com>
2025-09-25 15:05:26 -0400

    calculates price of 2 diff size sandwiches also accounting for underage and and senior discount (BETTER SOLUTION)

--- src/main/java/com/pluralsight/SandwichShop.java
+++ src/main/java/com/pluralsight/SandwichShop.java
@@ -10,28 +10,29 @@ public class SandwichShop {
         System.out.println("Choose size of the sandwich (1 or 2):");
         System.out.println("1: Regular: base price $5.45");
         System.out.println("2: Large: base price $8.95");
-
         int size = shop.nextInt();
 
         System.out.println("Whats your age?");
         int age = shop.nextInt();
 
-        if (size == 1){
-            if (age<=17){
-                System.out.println("Total Price $"+5.45*(1-0.10));
-            } else if (age>=65) {
-                System.out.println("Total Price $"+5.45*(1-0.20));
-            }
-            else System.out.println("Total Price $"+5.45);
+        double discount = 0.00;
+        if (age <=17){
+            discount = 0.10;
+        } else if (age >=65) {
+            discount = 0.20;
         }
-        if (size == 2){
-            if (age<=17){
-                System.out.println("Total Price $"+8.95*(1-0.10));
-            } else if (age>=65) {
-                System.out.println("Total Price $"+8.95*(1-0.20));
-            }
-            else System.out.println("Total Price $8.95");
+
+        double basePrice = 0.00;
+        if (size==1){
+            basePrice = 5.45;
+        } else if (size==2) {
+            basePrice = 8.95;
         }
+        else System.out.println("Invalid Size");
+
+        double finalPrice = basePrice * (1-discount);
+        System.out.println("Total: $" + finalPrice);
+
 
 
 
